fde5bca064b7ddeeff5b4e0b2c17cbb6a9038751
krish512 <krish512@hotmail.com>
2021-03-11 17:13:39 +0530

    Added more fields to logs

--- pkg/sink/ambassador.go
+++ pkg/sink/ambassador.go
@@ -6,6 +6,11 @@ import (
 	v2 "github.com/envoyproxy/go-control-plane/envoy/service/accesslog/v2"
 )
 
+type socketAddress struct {
+	Address string `json:"address"`
+	Port    uint64 `json:"port"`
+}
+
 type requestFields struct {
 	HeadersBytes uint64 `json:"headersBytes"`
 	BodyBytes    uint64 `json:"bodyBytes"`
@@ -27,21 +32,27 @@ type responseFields struct {
 
 // AmbassadorLog is struct for JSON format to push ambassador access logs to Elasticsearch
 type AmbassadorLog struct {
-	Timestamp       string         `json:"timestamp"`
-	TraceID         string         `json:"traceId"`
-	Method          string         `json:"method"`
-	Protocol        string         `json:"protocol"`
-	Scheme          string         `json:"scheme"`
-	StatusCode      uint32         `json:"statusCode"`
-	Domain          string         `json:"domain"`
-	Path            string         `json:"path"`
-	UserAgent       string         `json:"userAgent"`
-	Referer         string         `json:"referer"`
-	ForwardedFor    string         `json:"forwardedFor"`
-	UpstreamFailure string         `json:"upsteamFailure"`
-	Time            time.Duration  `json:"time_ms"`
-	Request         requestFields  `json:"request"`
-	Response        responseFields `json:"response"`
+	Timestamp                     string         `json:"timestamp"`
+	TraceID                       string         `json:"traceId"`
+	Method                        string         `json:"method"`
+	Protocol                      string         `json:"protocol"`
+	Scheme                        string         `json:"scheme"`
+	StatusCode                    uint32         `json:"statusCode"`
+	Domain                        string         `json:"domain"`
+	Path                          string         `json:"path"`
+	UserAgent                     string         `json:"userAgent"`
+	Referer                       string         `json:"referer"`
+	ForwardedFor                  string         `json:"forwardedFor"`
+	UpstreamFailure               string         `json:"upsteamFailure"`
+	UpstreamCluster               string         `json:"upstreamCluster"`
+	DownstreamDirectRemoteAddress socketAddress  `json:"downstreamDirectRemoteAddress"`
+	DownstreamRemoteAddress       socketAddress  `json:"downstreamRemoteAddress"`
+	DownstreamLocalAddress        socketAddress  `json:"downstreamLocalAddress"`
+	UpstreamRemoteAddress         socketAddress  `json:"upstreamRemoteAddress"`
+	UpstreamLocalAddress          socketAddress  `json:"upstreamLocalAddress"`
+	Time                          time.Duration  `json:"time_ms"`
+	Request                       requestFields  `json:"request"`
+	Response                      responseFields `json:"response"`
 }
 
 func transform(bundle *v2.StreamAccessLogsMessage_HTTPAccessLogEntries) []AmbassadorLog {
@@ -60,7 +71,28 @@ func transform(bundle *v2.StreamAccessLogsMessage_HTTPAccessLogEntries) []Ambass
 			Referer:         logEntry.Request.GetReferer(),
 			ForwardedFor:    logEntry.Request.GetForwardedFor(),
 			UpstreamFailure: logEntry.CommonProperties.GetUpstreamTransportFailureReason(),
-			Time:            logEntry.CommonProperties.GetTimeToLastDownstreamTxByte().AsDuration() / time.Millisecond,
+			UpstreamCluster: logEntry.CommonProperties.GetUpstreamCluster(),
+			DownstreamDirectRemoteAddress: socketAddress{
+				Address: logEntry.CommonProperties.GetDownstreamDirectRemoteAddress().GetSocketAddress().GetAddress(),
+				Port:    uint64(logEntry.CommonProperties.GetDownstreamDirectRemoteAddress().GetSocketAddress().GetPortValue()),
+			},
+			DownstreamRemoteAddress: socketAddress{
+				Address: logEntry.CommonProperties.GetDownstreamRemoteAddress().GetSocketAddress().GetAddress(),
+				Port:    uint64(logEntry.CommonProperties.GetDownstreamRemoteAddress().GetSocketAddress().GetPortValue()),
+			},
+			DownstreamLocalAddress: socketAddress{
+				Address: logEntry.CommonProperties.GetDownstreamLocalAddress().GetSocketAddress().GetAddress(),
+				Port:    uint64(logEntry.CommonProperties.GetDownstreamLocalAddress().GetSocketAddress().GetPortValue()),
+			},
+			UpstreamRemoteAddress: socketAddress{
+				Address: logEntry.CommonProperties.GetUpstreamRemoteAddress().GetSocketAddress().GetAddress(),
+				Port:    uint64(logEntry.CommonProperties.GetUpstreamRemoteAddress().GetSocketAddress().GetPortValue()),
+			},
+			UpstreamLocalAddress: socketAddress{
+				Address: logEntry.CommonProperties.GetUpstreamLocalAddress().GetSocketAddress().GetAddress(),
+				Port:    uint64(logEntry.CommonProperties.GetUpstreamLocalAddress().GetSocketAddress().GetPortValue()),
+			},
+			Time: logEntry.CommonProperties.GetTimeToLastDownstreamTxByte().AsDuration() / time.Millisecond,
 			Request: requestFields{
 				HeadersBytes: logEntry.Request.GetRequestHeadersBytes(),
 				BodyBytes:    logEntry.Request.GetRequestBodyBytes(),
